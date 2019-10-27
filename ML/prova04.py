6e31581243f751f08d4f7b45a1017cccf3859d28
Francesco-Piccoli <56897188+Francesco-Piccoli@users.noreply.github.com>
2019-10-27 00:00:44 -0700

    first try to merge
    merging

--- ML/prova04.py
+++ ML/prova04.py
@@ -39,23 +39,44 @@ client = language.LanguageServiceClient()
 
 tweets = pd.read_csv('C:\\Users\\Francesco\\PycharmProjects\\PGAA1819\\Cal_hacks\\ML\\sentiment.csv', sep=";")
 print(tweets.head(20))
-# The text to analyze
-# text = 'I really love Berkeley, it is amazing. I like it a lot. Super cool, I am so excited to tell everybody!'
-# document = types.Document(
-#     content=text,
-#     type=enums.Document.Type.PLAIN_TEXT)
-# annotations = client.analyze_sentiment(document=document)
-#
-# # Detects the sentiment of the text
-# sentiment = client.analyze_sentiment(document=document).document_sentiment
-# Entities = client.analyze_entities(document=document)
-#
-# print('Text: {}'.format(text))
+
+tweets_str = tweets.to_numpy()
+
+print(tweets_str)
+sentiments = []
+for tweet_str in tweets_str:
+    document = types.Document(
+        content=str(tweet_str),
+        type=enums.Document.Type.PLAIN_TEXT, language = "en")
+    #annotations = client.analyze_sentiment(document=document)
+
+    # Detects the sentiment of the text
+    sentiments.append(client.analyze_sentiment(document=document).document_sentiment)
+    #Entities = client.analyze_entities(document=document)
+    #sentence_sentiment.append(sentence.sentiment.score)
+
+
+#print(sentiments)
+#sentiment = np.asarray(sentiments)
+magnitudes = []
+scores = []
+for sentiment in sentiments:
+    magnitudes.append(sentiment.magnitude)
+    scores.append(sentiment.score)
+
+
+print(magnitudes)
+print(scores)
+
+
+# print('Text: {}'.format(tweets_str))
 # print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
-#
+#sentence_sentiment = []
 # for index, sentence in enumerate(annotations.sentences):
-#     sentence_sentiment = sentence.sentiment.score
-#     print('Sentence {} has a sentiment score of {}'.format(index, sentence_sentiment))
+#     sentence_sentiment.append(sentence.sentiment.score)
+#     print('Sentence {} has a sentiment score of {}'.format(index, sentence.sentiment.score))
+
+
 #
 # print('Overall Sentiment: score of {} with magnitude of {}'.format(sentiment.score, sentiment.magnitude))
 # ######################################################################################################################
@@ -81,41 +102,49 @@ print(tweets.head(20))
 # # #
 # # # print(data.groupby('not.fully.paid').mean())
 # #
-# X = data.loc[:, data.columns != 'Burnout']
-# Y = data.loc[:, data.columns == 'Burnout']
-# #
-# validation_size = 0.30
-# seed = 142
-# X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+
+# ##################################################################################################################
+
+
+data["magnitude"] = magnitudes
+data["score"] = scores
+
+print(data.head(20))
+
+X = data.loc[:, data.columns != 'Burnout']
+Y = data.loc[:, data.columns == 'Burnout']
+validation_size = 0.30
+seed = 142
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
 #
+
+logit_model=sm.Logit(Y_train,X_train)
+result=logit_model.fit()
+print(result.summary2())
 #
-# logit_model=sm.Logit(Y_train,X_train)
-# result=logit_model.fit()
-# print(result.summary2())
-# #
-#
-# X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=seed)
-# logreg = LogisticRegression()
-# result = logreg.fit(X_train, y_train)
-#
-# y_pred = logreg.predict(X_test)
-# proba = logreg.predict_proba(X_test)
-# print(proba[:,1])
-# plt.hist(proba[:,1])
-# #plt.plot(X_test, proba[:,0])
-# plt.show()
-# confusion_matrix = confusion_matrix(y_test, y_pred)
-# print(confusion_matrix)
-# print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(logreg.score(X_test, y_test)))
-#
-#
-# #new_obs = [[1,0,22, 0.7,1]]
-# new_obs = [[0,1,70, 1,0]]
-# # Create the pandas DataFrame
-# new_observation = pd.DataFrame(new_obs, columns=['Insomnia', 'Gender', 'Age', 'emails_read', 'seasonality'])
-# print(new_observation.size)
-#
-# new_prediction = logreg.predict_proba(new_observation)
-# print(new_prediction[:,1])
-#
+
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=seed)
+logreg = LogisticRegression()
+result = logreg.fit(X_train, y_train)
+
+y_pred = logreg.predict(X_test)
+proba = logreg.predict_proba(X_test)
+print(proba[:,1])
+plt.hist(proba[:,1])
+#plt.plot(X_test, proba[:,0])
+plt.show()
+confusion_matrix = confusion_matrix(y_test, y_pred)
+print(confusion_matrix)
+print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(logreg.score(X_test, y_test)))
+
+
+#new_obs = [[1,0,22, 0.7,1, 0.7, -0.5]]
+new_obs = [[0,1,70, 1,0, 1, 0.4]]
+# Create the pandas DataFrame
+new_observation = pd.DataFrame(new_obs, columns=['Insomnia', 'Gender', 'Age', 'emails_read', 'seasonality', 'magnitude', 'score'])
+print(new_observation.size)
+
+new_prediction = logreg.predict_proba(new_observation)
+print(new_prediction[:,1])
+
 
